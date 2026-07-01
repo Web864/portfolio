14ebada6c3bba02e87922088ebd5d27f50a1706f
Career-dot <rabia.naz@career.edu.pk>
2026-07-01 16:24:53 +0500

    responsive

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='bg-black text-[var(--muted)] text-center p2'>
+    <footer className='bg-black text-[var(--muted)] text-center p-2'>
         <p >Designed & built by <span className='text-[var(--text)]'>Rabia Naz</span> · © 2026 · All rights reserved</p>
     </footer>
   )
